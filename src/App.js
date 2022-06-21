87e896a2b6c4347d34fb506e63af5fd7b8bf1787
Hassan Bulega <hassanbulega@gmail.com>
2022-06-21 16:02:53 +0300

    feat: add app functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>need alternative text generator?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs: </label>
+        <input
+          type='number'
+          //  min={1}
+          //  max={15}
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
